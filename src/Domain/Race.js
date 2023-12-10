fd0ef452f1e0282edd4e2cf952dd1caa27a78db8
haesa <xdhaesa@gmail.com>
2023-12-10 17:39:26 +0900

    [Refactor]: cars 데이터 클래스 밖으로 분리
    cars 데이터가 Race 클래스 외에도 Winner 클래스에도 필요해서 상위 클래스(App)로 분리함

--- src/Domain/Race.js
+++ src/Domain/Race.js
@@ -1,22 +1,12 @@
 import createRandomNumber from '../random';
 
-class Race {
-  #cars;
-
-  constructor(names) {
-    this.#cars = names.map((name) => ({ name, distance: 0 }));
-  }
-
-  race() {
-    this.#cars = this.#cars.map((car) =>
-      Race.forward(car, createRandomNumber())
-    );
-    return [...this.#cars];
-  }
-
-  static forward(car, randomNumber) {
+const Race = {
+  race(cars) {
+    return cars.map((car) => Race.forward(car, createRandomNumber()));
+  },
+  forward(car, randomNumber) {
     return randomNumber > 4 ? { ...car, distance: car.distance + 1 } : car;
-  }
-}
+  },
+};
 
 export default Race;
